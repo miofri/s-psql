ba8017aa3caa7d34e9c9d13b8a372df8482dca49
selvi <slvkesuma@gmail.com>
2024-04-28 22:39:38 +0300

    added some other queries from user router, added put route for passw change

--- controllers/queries.js
+++ controllers/queries.js
@@ -1,4 +1,10 @@
 const findUserByEmail = `SELECT * FROM users WHERE email = $1`;
+
+//user router
+const createUser = `INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *`;
+const updatePasswordByEmail = `UPDATE users SET password = $1 WHERE email = $2`;
+
+//blog router
 const selectPostsByid = `SELECT * FROM blogs WHERE user_id = $1`;
 const createNewBlog = `INSERT INTO blogs (title, body, user_id) VALUES ($1, $2, $3) RETURNING *`;
 const updateBlogById = `UPDATE blogs SET title = $1, body = $2, date = $3 WHERE id = $4 RETURNING *`;
@@ -6,6 +12,8 @@ const deleteBlogById = `DELETE FROM blogs WHERE id = $1`;
 
 module.exports = {
 	findUserByEmail,
+	createUser,
+	updatePasswordByEmail,
 	selectPostsByid,
 	createNewBlog,
 	updateBlogById,
--- controllers/usersRouter.js
+++ controllers/usersRouter.js
@@ -3,6 +3,7 @@ const { pool } = require('../db/db');
 const bcrypt = require('bcrypt');
 const headerCheck_mw = require('./middlewares/headerCheck_mw');
 const saltRounds = 10;
+const queries = require('./queries');
 
 usersRouter.post('/signup', async (req, res) => {
 	bcrypt.hash(req.body.password, saltRounds, async function (error, hash) {
@@ -10,10 +11,10 @@ usersRouter.post('/signup', async (req, res) => {
 			next(error);
 		}
 		try {
-			const query = await pool.query(
-				`INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *`,
-				[req.body.email, hash]
-			);
+			const query = await pool.query(queries.createUser, [
+				req.body.email,
+				hash,
+			]);
 			res.sendStatus(201);
 		} catch (error) {
 			console.error('Error executing query:', error);
@@ -30,10 +31,11 @@ usersRouter.put('/:userid', headerCheck_mw, async (req, res) => {
 			next(error);
 		}
 		try {
-			const query = await pool.query(
-				`UPDATE users SET password = $1 WHERE email = $2`,
-				[hash, req.user.email]
-			);
+			const query = await pool.query(queries.updatePasswordByEmail, [
+				hash,
+				req.user.email,
+			]);
+			res.sendStatus(200);
 		} catch (error) {
 			next(error);
 		}
