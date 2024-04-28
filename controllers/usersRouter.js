28ff22e73fadd2f199dff7cd620269c12c667f1f
selvi <slvkesuma@gmail.com>
2024-04-28 11:54:39 +0300

    created basic signup route

--- controllers/usersRouter.js
+++ controllers/usersRouter.js
@@ -1,4 +1,25 @@
 const usersRouter = require('express').Router();
 const { pool } = require('../db/db');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
+usersRouter.post('/signup', async (req, res) => {
+	try {
+		const email = req.body.email;
+		const password = req.body.password;
+		bcrypt.hash(password, saltRounds, async function (err, hash) {
+			const query = await pool.query(
+				`INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *`,
+				[email, hash]
+			);
+			console.log(query);
+			res.sendStatus(201);
+		});
+	} catch (error) {
+		console.error('Error executing query:', error);
+		res
+			.status(500)
+			.json({ message: 'Internal server error: Registration failed' });
+	}
+});
 module.exports = usersRouter;
