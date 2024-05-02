29c0c009299b44fa8d6a7d1a4f4cefe9acf09a24
selvi <slvkesuma@gmail.com>
2024-05-02 15:05:23 +0300

    deleted some spacing|

--- controllers/middlewares/auth_mw.js
+++ controllers/middlewares/auth_mw.js
@@ -10,13 +10,11 @@ const auth_mw = async (req, res, next) => {
 	}
 	const email = req.body.email.toLowerCase();
 	const password = req.body.password;
-
 	const found = await pool.query(findUserByEmail, [email]);
 
 	if (found.rows.length === 0) {
 		return res.status(404).json({ message: 'User not found' });
 	}
-
 	const passwordHash = found.rows[0].password;
 	const match = await bcrypt.compare(password, passwordHash);
 	console.log(match);
@@ -24,7 +22,6 @@ const auth_mw = async (req, res, next) => {
 	if (!match) {
 		return res.status(401).json({ message: 'Login failed - unathourized' });
 	}
-
 	req.user = { email: found.rows[0].email, user_id: found.rows[0].id };
 	next();
 };
